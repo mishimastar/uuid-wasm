35d61fa91aaeebb18ecea21ffc851cd089249684
Sergey Saltykov <s.saltykov@group-ib.com>
2023-10-14 21:35:47 +0700

    +

--- src/main.ts
+++ src/main.ts
@@ -36,11 +36,29 @@ export const InitUUID = async (options = opts): Promise<UUIDModule> => {
         return new Date(Number(getMilliseconds()));
     };
 
+    const alphabet: number[] = [];
+    for (let i = 48; i < 58; i++) alphabet[i] = i;
+    for (let i = 65; i < 71; i++) alphabet[i] = i + 32;
+    for (let i = 97; i < 103; i++) alphabet[i] = i;
     const timeFromV1 = (uuid: string): Date => {
         if (typeof uuid !== 'string' || uuid.length !== 36) TypeError('uuid is not UUID v1', { cause: { uuid } });
         if (uuid[14] !== '1') TypeError('uuid is not UUID v1', { cause: { uuid } });
-        const u = uuid.toLowerCase();
-        return unsafeTimeFromV1(u);
+        pasteShiftChar(28, alphabet[uuid.charCodeAt(0)]!);
+        pasteShiftChar(24, alphabet[uuid.charCodeAt(1)]!);
+        pasteShiftChar(20, alphabet[uuid.charCodeAt(2)]!);
+        pasteShiftChar(16, alphabet[uuid.charCodeAt(3)]!);
+        pasteShiftChar(12, alphabet[uuid.charCodeAt(4)]!);
+        pasteShiftChar(8, alphabet[uuid.charCodeAt(5)]!);
+        pasteShiftChar(4, alphabet[uuid.charCodeAt(6)]!);
+        pasteShiftChar(0, alphabet[uuid.charCodeAt(7)]!);
+        pasteShiftChar(44, alphabet[uuid.charCodeAt(9)]!);
+        pasteShiftChar(40, alphabet[uuid.charCodeAt(10)]!);
+        pasteShiftChar(36, alphabet[uuid.charCodeAt(11)]!);
+        pasteShiftChar(32, alphabet[uuid.charCodeAt(12)]!);
+        pasteShiftChar(56, alphabet[uuid.charCodeAt(15)]!);
+        pasteShiftChar(52, alphabet[uuid.charCodeAt(16)]!);
+        pasteShiftChar(48, alphabet[uuid.charCodeAt(17)]!);
+        return new Date(Number(getMilliseconds()));
     };
 
     return { ...new UUIDGen(wasi, instanceGen), unsafeTimeFromV1, timeFromV1 };
